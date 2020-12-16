9b3f1ed0d6265c23843cf27a72f6ea64e943a804
Josh Grossman <grossmanjoshuaj@gmail.com>
2020-12-16 08:53:37 -0600

    remove auth

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import { BrowserRouter } from 'react-router-dom'
 import 'semantic-ui-css/semantic.min.css'
 import { Provider } from 'react-redux'
 import { configurationStore } from './store/configurestore';
-import Authenticator from '../src/components/authenticator'
+
 
 
 const store = configurationStore();
@@ -13,13 +13,13 @@ const store = configurationStore();
 
 
 ReactDOM.render(
-    <Authenticator>
+    
         <Provider store={store}>
         <BrowserRouter>
             <App />
         </BrowserRouter>
     </Provider>
-    </Authenticator>
+  
     
 
     , document.querySelector('#root'));
